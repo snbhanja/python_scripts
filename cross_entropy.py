87c1eda8144475ed467ef7bc62a7d6300274c72b
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2017-10-02 11:56:05 +0530

    Update cross_entropy.py

--- cross_entropy.py
+++ cross_entropy.py
@@ -3,7 +3,10 @@ import numpy as np
 def cross_entropy(Y, P):
     # Write a function that takes as input two lists Y, P,
     # and returns the float corresponding to their cross-entropy.
-    # Y is the labels and P is the predicted probabilities.
+    # Y is the true labels and P is the predicted probabilities.
     Y = np.float_(Y)
     P = np.float_(P)
     return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
+
+# Reference:-
+# https://datascience.stackexchange.com/questions/9302/the-cross-entropy-error-function-in-neural-networks
