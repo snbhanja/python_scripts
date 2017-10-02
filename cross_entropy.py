d9c273a4d64a946f42b9eda1d7bc9df7f06cffba
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2017-10-02 11:45:37 +0530

    Update cross_entropy.py

--- cross_entropy.py
+++ cross_entropy.py
@@ -3,6 +3,7 @@ import numpy as np
 def cross_entropy(Y, P):
     # Write a function that takes as input two lists Y, P,
     # and returns the float corresponding to their cross-entropy.
+    # Y is the labels and P is the predicted probabilities.
     Y = np.float_(Y)
     P = np.float_(P)
     return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
